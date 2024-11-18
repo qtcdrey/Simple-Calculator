75b23edf4cbda96f5431077a36887d3eb0dbceaa
qtcdrey <qtcdrey@users.noreply.github.com>
2024-11-18 09:58:38 +0800

    organize function called operation

--- simple_calculator.c
+++ simple_calculator.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+float operation(char op);
+
+float num1,num2,result;
+char op;
+
+int main() {
+    
+    printf("Enter a Number: ");
+    scanf("%f",&num1);
+    printf("( +, -, *, / ): ");
+    scanf("%s",&op);
+    printf("Enter a Number: ");
+    scanf("%f",&num2);
+    operation(op);
+    printf("%.2f",result);
+
+    return 0;
+}
+
+float operation(char op) {
+
+    if (op == '+') {
+        result = num1 + num2;
+    } else if (op == '-') {
+        result = num1 - num2;
+    } else if (op == '*') {
+        result = num1 * num2;
+    } else if (op == '/') {
+        if (num2 != 0) {
+            result = num1 / num2;
+        } else {
+            puts("error");
+            result = 0;
+        }
+    } else {
+        puts("invalid operator");
+        result = 0;
+    }
+
+    return result;
+}
