6c6d65cc8a58fc1f7b02a4a3bc8d66cbe5ef5bf4
qtcdrey <andreydelrosario8@gmail.com>
2024-11-21 20:40:24 +0800

    if to switch statement and add a math.h library

--- simple_calculator.c
+++ simple_calculator.c
@@ -1,42 +1,70 @@
 #include <stdio.h>
+#include <math.h>
 
 float operation(char op);
 
 float num1,num2,result;
 char op;
 
-int main() {
-    
-    printf("Enter a Number: ");
+int main() 
+{
+    printf("Enter a Number  : ");
     scanf("%f",&num1);
-    printf("( +, -, *, / ): ");
+    printf("(+,-,*,/,%%,#,^) : ");
     scanf("%s",&op);
-    printf("Enter a Number: ");
-    scanf("%f",&num2);
+    
+    if (op != '#') 
+    {
+        printf("Enter a Number  : ");
+        scanf("%f",&num2);
+    }
+    
     operation(op);
     printf("%.2f",result);
-
+    
     return 0;
 }
 
-float operation(char op) {
-
-    if (op == '+') {
-        result = num1 + num2;
-    } else if (op == '-') {
-        result = num1 - num2;
-    } else if (op == '*') {
-        result = num1 * num2;
-    } else if (op == '/') {
-        if (num2 != 0) {
-            result = num1 / num2;
-        } else {
-            puts("error");
-            result = 0;
-        }
-    } else {
-        puts("invalid operator");
-        result = 0;
+float operation(char op) 
+{
+    switch(op) 
+    {
+        case '+':
+            result =  num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } 
+            else {
+                puts("error");
+            }
+            break;
+        case '%':
+            if (num2 != 0) {
+                result = (int)num1 % (int)num2;
+            } else {
+                puts("error");
+            }
+            break;
+        case '#':
+            if (num1 >= 0) {
+                result = sqrt(num1);
+            } else {
+                puts("negative numbers");
+            }
+            break;
+        case '^':
+            result = pow(num1,num2);
+            break;
+        default:
+            puts("invalid operator");
     }
 
     return result;
